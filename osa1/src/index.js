9bb646ceffca021e14564fb3d9358213cc6fd67b
exel80 <theexel80@gmail.com>
2019-05-09 17:21:39 +0300

    Tehtävä 1.5 tehty

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,73 +1,75 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
 
-const Content = (props) => {
-
-    // Puskee kaikki parts objectissa olevat omaan listaan
-    const partsList = []
-    for (let i = 0; i < props.parts.length; i++) {
-        partsList.push(<Part key={i} part={props.parts[i]} />)
-    }
-
-    return (
-        <div>
-            {partsList}
-            {/* <Part part={props.parts.parts[0]} />
-            <Part part={props.parts.parts[0]} />
-            <Part part={props.parts.parts[0]} /> */}
-        </div>
-    )
-}
-
-const Part = (props) => {
-    return (
-        <p>
-            {props.part.name} {props.part.exercises}
-        </p>
-    )
-}
-
-const Total = (props) => {
-    return (
-        <p>
-            Yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} tehtävää
-        </p>
-    )
-}
-
-const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-
-  const parts = [
-    {
-      name: 'Reactin perusteet',
-      exercises: 10
-    },
-    {
-      name: 'Tiedonvälitys propseilla',
-      exercises: 7
-    },
-    {
-      name: 'Komponenttien tila',
-      exercises: 14
-    }
-  ]
+const Content = props => {
+  // Puskee kaikki parts objectissa olevat omaan listaan
+  const partsList = [];
+  for (let i = 0; i < props.parts.length; i++) {
+    partsList.push(<Part key={i} part={props.parts[i]} />);
+  }
 
   return (
     <div>
-        <Header course={course} />
-
-        <Content parts={parts} />
-
-        <Total parts={parts} />
+      {partsList}
+      {/* <Part part={props.parts.parts[0]} />
+            <Part part={props.parts.parts[0]} />
+            <Part part={props.parts.parts[0]} /> */}
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Part = props => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Total = props => {
+  return (
+    <p>
+      Yhteensä{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}{" "}
+      tehtävää
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack -sovelluskehitys",
+    parts: [
+      {
+        name: "Reactin perusteet",
+        exercises: 10
+      },
+      {
+        name: "Tiedonvälitys propseilla",
+        exercises: 7
+      },
+      {
+        name: "Komponenttien tila",
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+
+      <Content parts={course.parts} />
+
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
