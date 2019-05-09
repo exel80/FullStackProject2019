30c80a487c7c25d28015ed32957f75129c329e43
exel80 <theexel80@gmail.com>
2019-05-09 16:50:41 +0300

    Tehtävät 1.1 ja 1.2 tehty

---
+++ osa1/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.part1} />
+            <Part part={props.part2} />
+            <Part part={props.part3} />
+        </div>
+    )
+}
+
+const Part = (props) => {
+    const split = props.part.split(":")
+
+    return (
+        <p>
+            {split[0]} {split[1]}
+        </p>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>
+            Yhteensä {props.total} tehtävää
+        </p>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+
+  return (
+    <div>
+        <Header course={course} />
+
+        <Content part1={part1+":"+exercises1} part2={part2+":"+exercises2} part3={part3+":"+exercises3} />
+
+        <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
